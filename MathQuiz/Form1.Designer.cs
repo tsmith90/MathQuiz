65f1568796e36e8525c6803de8b922695abe882a
tsmith90 <tsmith90@my.westga.edu>
2021-01-08 13:30:40 +0800

    Set form size ad resize properties.

--- MathQuiz/Form1.Designer.cs
+++ MathQuiz/Form1.Designer.cs
@@ -35,7 +35,9 @@ namespace MathQuiz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Math Quiz";
             this.ResumeLayout(false);
